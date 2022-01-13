8279df9475b74c4b7bedac93adaa95725c8ac14c
ebosas <ebosas@gmail.com>
2022-01-13 23:00:16 +0200

    Add dbt_utils dependency

--- dbt_datapipeline.sh
+++ dbt_datapipeline.sh
@@ -3,5 +3,8 @@
 echo "[DBT command] debug"
 dbt debug --profiles-dir ./ --profile dbt_dev
 
+echo "[DBT command] deps"
+dbt deps --profiles-dir ./ --profile dbt_dev
+
 echo "[DBT command] run"
 dbt run --profiles-dir ./ --profile dbt_dev
